e7e1e0fd3a7bfdc788812c4035d1ab92a46a0579
michlans17 <jmichlan@uoregon.edu>
2017-03-04 00:38:12 -0800

    Added more scripts

---
+++ Assets/Scripts/Boom.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boom : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Asteroid")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ToggleGravity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGravity : MonoBehaviour {
+	
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		//Physics.gravity = 1;
+		print (rb2d.gravityScale);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			//Physics.gravity = Physics2D.gravity * -1;
+			rb2d.gravityScale *= -1;
+			print (rb2d.gravityScale);
+			print ("space pressed");
+		}
+
+	}
+}
